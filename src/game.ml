3127d74a5e853f19b2ba03a237d77e1dac3a790b
Bramford Horton <bram.horton@gmail.com>
2018-02-16 21:16:16 +1300

    Add basic world generation

--- src/game.ml
+++ src/game.ml
@@ -22,6 +22,22 @@ module Entity = struct
         | Good -> Notty.A.white
         | Neutral -> Notty.A.yellow
         | Bad -> Notty.A.red
+
+    let nature_of_int i =
+      match i with
+      | 1 -> Neutral
+      | 2 -> Bad
+      | _ -> Good
+
+    let random =
+      let nature =
+        Random.int 100 mod 2
+        |> nature_of_int
+      in
+      let age = Random.int 20 + 13 in
+      create
+        ~age:age
+        ~nature:nature
   end
 
   module Tree = struct
@@ -43,22 +59,52 @@ module Entity = struct
       match tree.breed with
         | Pine -> Notty.A.green
         | Birch -> Notty.A.lightgreen
+
+    let breed_of_int i =
+      match i with
+      | 1 -> Birch
+      | _ -> Pine
+
+    let random =
+      let breed =
+        Random.int 100 mod 1
+        |> breed_of_int
+      in
+      let age = Random.int 1000 + 1 in
+      create
+        ~age:age
+        ~breed:breed
   end
 
-  type entity =
+  type t =
     | Human of Human.t
     | Tree of Tree.t
 
-  let entity_fg e = function
+  let fg e = function
     | Human e -> Human.fg e
     | Tree e -> Tree.fg e
+
+  let random =
+    match Random.int 10 with
+    | 0 -> Human Human.random
+    | _ -> Tree Tree.random
+
+
+  let gen_list i =
+    let rec gl i l =
+      match l with
+      | [] | [_] -> l
+      | hd :: tl -> gl (i - 1) (random :: l)
+    in
+    gl i []
 end
 
 module World = struct
   type coord = (int * int)
 
   type t = {
-    map : coord List.t
+    coords : coord list;
+    entities : Entity.t list;
   }
 
   let create w h =
@@ -72,15 +118,11 @@ module World = struct
       l
     in
     let coords = coords_of_bounds 0 w 0 h [] in
-    { map = coords;
+    let entities = Entity.gen_list (w * h) in
+    { coords = coords;
+      entities = entities;
     }
 end
 
 let myworld =
   World.create 64 64
-
-let myhuman =
-  Entity.Human.create ~age:1 ~nature:Good
-
-let mytree =
-  Entity.Tree.create ~age:1000 ~breed:Pine
