fabc58dc0bd00781e85d2a84eb54f9a1307da399
Bramford Horton <bram.horton@gmail.com>
2018-04-02 19:20:23 +1200

    Handle keyboard input for player movement
    Also provides key binds for quitting

--- src/game.ml
+++ src/game.ml
@@ -396,7 +396,31 @@ module World = struct
 end
 
 let game_loop world =
-  World.draw_around_player world "player" |> World.render
+  let term = (Notty_unix.Term.create ~mouse:false ~bpaste:false ()) in
+  let player = "player" in
+  World.draw_around_player world player |> World.render term;
+  let rec event_loop term world player =
+    match (Notty_unix.Term.event term) with
+    | `Key (`ASCII 'h',_) ->
+      let world = World.move_player world player World.Left in
+      World.draw_around_player world player |> World.render term;
+      event_loop term world player
+    | `Key (`ASCII 'j',_) ->
+      let world = World.move_player world player World.Down in
+      World.draw_around_player world player |> World.render term;
+      event_loop term world player
+    | `Key (`ASCII 'k',_) ->
+      let world = World.move_player world player World.Up in
+      World.draw_around_player world player |> World.render term;
+      event_loop term world player
+    | `Key (`ASCII 'l',_) ->
+      let world = World.move_player world player World.Right in
+      World.draw_around_player world player |> World.render term;
+      event_loop term world player
+    | `Key (`Escape,_) | `Key (`ASCII 'q',_) | `Key (`ASCII 'C',[`Ctrl]) -> exit 0
+    | _ -> event_loop term world player
+  in
+  event_loop term world player
 
 let run_game mode =
   let open Core.Command in
@@ -408,8 +432,7 @@ let run_game mode =
   | Some "print_world" ->
     World.print world
   | Some _ | None ->
-    game_loop world;
-;;
+    game_loop world
 
 let command =
   let open Core.Command in
