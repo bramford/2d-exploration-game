1e8109f57ef6e3d7560a5f4519496d7470422009
Bramford Horton <bram.horton@gmail.com>
2018-09-25 12:41:42 +1200

    Add the new 'stick' item type
    Note: The new 'Stick' module is created using the new & improved
    `Item_lib.Make` functor

--- src/item.ml
+++ src/item.ml
@@ -3,19 +3,29 @@ module Rock = Item_lib.Make(struct
     let name = "Rock"
   end)
 
+module Stick = Item_lib.Make(struct
+    let symbol = "-"
+    let name = "Stick"
+  end)
+
 type t =
   | Rock of Rock.t
+  | Stick of Stick.t
 
 let draw = function
   | Some Rock _ -> Rock.draw
+  | Some Stick _ -> Stick.draw
   | None -> Notty.I.char Notty.A.empty ' ' 1 1
 
 let to_string = function
   | Some Rock r -> Rock.to_string r
+  | Some Stick r -> Stick.to_string r
   | None -> "None"
 
 let random i =
   match i mod 1000 with
   | x when x < 5 ->
     Some (Rock (Rock.create ~weight:((Random.int 4) + 1)))
+  | x when (x > 5 && x < 11) ->
+    Some (Stick (Stick.create ~weight:((Random.int 2) + 1)))
   | _ -> None
--- src/item.mli
+++ src/item.mli
@@ -1,7 +1,9 @@
 module Rock : Item_lib.S
+module Stick : Item_lib.S
 
 type t =
   | Rock of Rock.t
+  | Stick of Stick.t
 
 val draw : t option -> Notty.image
 val to_string : t option -> string
